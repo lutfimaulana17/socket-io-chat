1e3cf6900d0f921e8f9604ca92cc0ab1b358f600
Lutfi Maulana <lutfimaulana@192.168.1.6>
2022-07-24 21:13:05 +0700

    config socket io

--- server/index.js
+++ server/index.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const { Server } = requre('socket.io');
 
 app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(socket.id)
+
+    socket.on("disconect", () => {
+        console.log('User Disconected', socket.id)
+    })
+})
 
 server.listen(3001, () => {
     console.log('SERVER RUNNING')
